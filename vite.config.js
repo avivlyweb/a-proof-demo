1e834c5952183c6c6bbbd7c839927f353da9932d
Aviv <avivly@Avivs-MacBook-Air-2.local>
2026-02-16 08:16:47 +0100

    Allow Modal hostnames in Vite dev server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['.modal.host'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
